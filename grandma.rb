4b09491334d78c54951974779d188f493c05f1db
Tanya Bihag <tbihag@gmail.com>
2019-08-13 21:31:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,9 @@
 
 def speak_to_grandma = "HI GRANDMA"
   if speak_to_grandma == "HI GRANDMA".upcase
-    puts "Hi Sonny"
+    puts "NO, NOT SINCE 1938!"
+  else if speak_to_grandma == "I LOVE YOU GRANDMA"  
+    puts "I LOVE YOU TOO PUMPKIN"
   else 
     puts "HUH?! SPEAK UP, SONNY!"
   end  
